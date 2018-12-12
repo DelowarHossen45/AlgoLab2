7af7bd4eb9bcba93a4ee58012f0d67baf2bd3191
Delowar Hossen Apple <applecse17@gmail.com>
2018-12-12 18:41:42 +0600

    file check

---
+++ FileCheck.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	int in,i=1;
+	while(i<5)
+	{
+		cin>>in;
+		cout<<in<<" ";
+		i++;
+	}
+
+}
